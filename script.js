321f70b2ab60b5ec17ff657e33b90fc5477b4566
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-07-05 15:15:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const pEl = document.querySelectorAll("p");
+const h1El = document.querySelectorAll("h1");
+const body = document.querySelector("body");
 
 for (let i = 0; i < pEl.length; i++){
     const text = pEl[i].textContent
@@ -8,7 +10,13 @@ for (let i = 0; i < pEl.length; i++){
     const newEl = document.createElement("span")
     newEl.textContent = newString
     newEl.style.textDecoration = "underline blue"
-    newEl.style.fontSize = "30px"
+    newEl.style.fontSize = "22px"
     pEl[i].appendChild(newEl)
-    pEl[i].style.fontSize = "30px"
+    pEl[i].style.fontSize = "22px"
 }
+
+for (let i = 0; i < h1El.length; i++) {
+    h1El[i].style.fontSize = "22px"
+}
+
+body.style.padding = "20px"
